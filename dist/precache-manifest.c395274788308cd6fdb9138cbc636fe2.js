49c86e5a98353588f85d43ecf36c06102eb7d788
rfkeepers <pinderlogic@gmail.com>
2020-12-06 19:48:02 -0700

    testing cpanel stuffs with dist

---
+++ dist/precache-manifest.c395274788308cd6fdb9138cbc636fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f9b0955d7b642b24d8f593f21a9eb7",
+    "url": "/aweoftheweek.pdf"
+  },
+  {
+    "revision": "586e5ac5d65e4b217ce3",
+    "url": "/css/app.04cfbdff.css"
+  },
+  {
+    "revision": "323ddf0cac87e7111af5f856c3515b0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "586e5ac5d65e4b217ce3",
+    "url": "/js/app.ea445906.js"
+  },
+  {
+    "revision": "06c1043eae80759569d1",
+    "url": "/js/chunk-vendors.d4014124.js"
+  },
+  {
+    "revision": "3a3f8bcf9b17c9a74ffa0c27d5af613e",
+    "url": "/manifest.json"
+  }
+]);
