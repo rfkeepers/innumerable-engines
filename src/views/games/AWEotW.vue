3a7b9e114bb1e143bd09372c178837cc1ecd660a
rfkeepers <pinderlogic@gmail.com>
2020-12-28 08:15:49 -0700

    update pdf, improve awe page

--- src/views/games/AWEotW.vue
+++ src/views/games/AWEotW.vue
@@ -1,9 +1,17 @@
 <template>
 <div class="aweotw">
     <h1 class="aweotw__title">Altered World Event of the Week</h1>
-    <div>A Monster of the Week derivative aimed at providing players a game environment to match SCP themes and the world of Remedy's 2019 game Control.</div>
+    <div class="aweotw__blurb">A Monster of the Week conversion for games specifically interested in the setting of "government agency containing the paranormal" as found in the SCP foundation or the world of Remedy's 2019 game Control.  What's included is a host of retooled playbooks, suggestions for centralizing episodic play around paranormal events instead of monsters, and guides for generating your table's own paranormal agency.</div>
     <br>
-    <div>View the pdf <a href="./aweoftheweek.pdf" target="_blank">HERE</a>.</div>
+    <div class="aweotw__download"><a href="./aweoftheweek.pdf" target="_blank">Download the PDF</a></div>
+    <div class="aweotw__changelog">
+      <h2>Changelog:</h2>
+      <div>2020.12.27:</div>
+      <div>Typo fix on the Psychic playbook, referring to the Director when it shouldn't.</div>
+      <br>
+      <div>2020.12.05:</div>
+      <div>Beta Release</div>
+    </div>
 </div>
 </template>
 
@@ -55,5 +63,41 @@ export default aweotw;
       }
     }
   }
+
+  &__download {
+    font-size: 1.5em;
+  }
+
+  &__blurb {
+    text-align: center;
+  }
+
+  &__changelog {
+    margin-top: 24px;
+    text-align: left;
+    width: 100%;
+
+    &:after {
+      content: "";
+      display: block;
+      width: 100%;
+      height: 1px;
+      background: $color-text;
+      position: relative;
+      top: 8px;
+    }
+
+    &:before {
+      content: "";
+      display: block;
+      width: 100%;
+      height: 1px;
+      background: $color-text;
+      position: relative;
+      top: 8px;
+    }
+  }
+
+
 }
 </style>
