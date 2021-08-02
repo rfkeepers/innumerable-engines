af8f2724b04aad6995f902353822482463c2a7a8
rfkeepers <pinderlogic@gmail.com>
2021-08-01 21:44:50 -0600

    ditch webpack for vite

--- src/views/site/Home.vue
+++ src/views/site/Home.vue
@@ -3,21 +3,21 @@
     <div class="section">
       <br>
       <h1 class="section__title" @click="routeTo('/aweoftheweek')">Altered World Event of the Week</h1>
-      <div class="section__blurb">A Monster of the Week conversion for games specifically interested in the setting of "government agency containing the paranormal" as found in the SCP foundation or the world of Remedy's 2019 game Control.  What's included is a host of retooled playbooks, suggestions for centralizing episodic play around paranormal events instead of monsters, and guides for generating your table's own paranormal agency.</div>
+      <div class="blurb">A Monster of the Week conversion for games specifically interested in the setting of "government agency containing the paranormal" as found in the SCP foundation or the world of Remedy's 2019 game Control.  What's included is a host of retooled playbooks, suggestions for centralizing episodic play around paranormal events instead of monsters, and guides for generating your table's own paranormal agency.</div>
       <br>
       <div class="section__download"><a href="./aweoftheweek.pdf" target="_blank">Download the PDF</a></div>
       <br>
       <br>
 
       <h1 class="section__title" @click="routeTo('/dwplaybooks')">Dungeon World Playbook Template</h1>
-      <div class="section__blurb">Want to create your own Dungeon World playbook?  We created an open source Latex document for quick, painless playbook typesetting. All you have to do is copy and paste the names and descriptions of your moves.  Give it a try!</div>
+      <div class="blurb">Want to create your own Dungeon World playbook?  We created an open source Latex document for quick, painless playbook typesetting. All you have to do is copy and paste the names and descriptions of your moves.  Give it a try!</div>
       <br>
       <div class="section__download"><a href="https://github.com/rfkeepers/dw-playbooks-tex">Github Repository</a></div>
       <br>
       <br>
 
       <h1 class="section__title" @click="routeTo('/dwplaybooks')">Dungeon World Custom Playbooks</h1>
-      <div class="section__blurb">Six custom playbooks for explosive mischief, nightmareish curses, and growing your own undead buddy.</div>
+      <div class="blurb">Six custom playbooks for explosive mischief, nightmareish curses, and growing your own undead buddy.</div>
       <br>
       <div class="section__download">
         <a href="./dwplaybooks/nightwatch">Nightwatch</a> -
@@ -106,10 +106,6 @@ export default home;
 
   &__download {
     font-size: 1.5em;
-  }
-
-  &__blurb {
-    text-align: center;
   }
 
 }
